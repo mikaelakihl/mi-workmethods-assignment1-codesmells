aa5146a536a2393aa926bda234318424329e95c0
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-01-17 15:56:50 +0100

    refactor: removed console.log

--- src/toggleDarkmode.ts
+++ src/toggleDarkmode.ts
@@ -2,16 +2,14 @@ const toggleLightModeButton = document.querySelector('.toggle_btn'); // Välj lj
 
 toggleLightModeButton!.addEventListener('click', toggleLightMode);
 
-console.log(toggleLightModeButton);
-
 export function toggleLightMode() {
   document.body.classList.toggle('darkmode');
+
   if (document.body.classList.contains('darkmode')) {
-    toggleLightModeButton.innerHTML = 'Välj mörkt läge';
-    console.log('mörkt läge');
+    toggleLightModeButton!.innerHTML = 'Välj mörkt läge';
+
   } else {
-    toggleLightModeButton.innerHTML = 'Välj ljust läge';
-    console.log('ljust läge');
+    toggleLightModeButton!.innerHTML = 'Välj ljust läge';
   }
 }
 
